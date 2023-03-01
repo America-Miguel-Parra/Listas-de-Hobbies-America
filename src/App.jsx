deef36d8f89042ae45c5b49fdec4c131b01fb777
America-Miguel-Parra <isc19350307@gmail.com>
2023-03-01 09:26:25 -0600

    Lista de Hobbies

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+  const nombre = "America Miguel Parra";
+
+  return (
+    <div>
+      <h3>Lista de Hobbies America</h3>
+      <ol>
+        <li>Juagar video juegos</li>
+        <li>Escuchar musica</li>
+        <li>Dormir</li>
+      </ol>
+    </div>
+  )
+
+}
+
+export default App
